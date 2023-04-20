2e2072a1da9b4f50aa34fe623c71dc0ffff7ff4a
Timbar09 <90119799+Timbar09@users.noreply.github.com>
2023-04-20 12:56:38 +0200

    Add unit tests for fizzbuzz method

---
+++ spec/fizzbuzz_spec.rb
@@ -0,0 +1,29 @@
+require './solver'
+
+describe Solver do
+  describe "#fizzbuzz" do
+    context ": When fizzbuzz method is called. it " do
+      solver = Solver.new
+
+      it "should return a string" do
+        expect(solver.fizzbuzz(1)).to be_a_kind_of(String)
+      end
+
+      it "should return 'Fizz' for 3" do
+        expect(solver.fizzbuzz(3)).to eq("Fizz")
+      end
+
+      it "should return 'Buzz' for 5" do
+        expect(solver.fizzbuzz(5)).to eq("Buzz")
+      end
+
+      it "should return 'FizzBuzz' for 15" do
+        expect(solver.fizzbuzz(15)).to eq("FizzBuzz")
+      end
+
+      it "should return 'FizzBuzz' for 30" do
+        expect(solver.fizzbuzz(30)).to eq("FizzBuzz")
+      end
+    end
+  end
+end
