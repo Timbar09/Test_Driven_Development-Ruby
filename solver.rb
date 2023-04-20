0e44b8b734b5545ca19b8045f0d29c1ff59972aa
fitz95 <fitzgeraldnsong@gmail.com>
2023-04-20 11:21:22 +0100

    Add Factorial method to Solver Class

---
+++ solver.rb
@@ -0,0 +1,11 @@
+class Solver
+    def factorial(n)
+        if n < 0
+            raise ArgumentError
+        end
+        if n == 0
+            return 1
+        end
+        return n * factorial(n - 1)
+    end
+end
--- spec/factorial_spec.rb
+++ spec/factorial_spec.rb
@@ -3,25 +3,26 @@ require './solver'
 describe Solver do
   describe "#factorial" do
     context ": When factorial method is called. it " do
+      solver = Solver.new
 
       it "should return a number" do
-        expect(Solver.factorial(5)).to be_a_kind_of(Integer)
+        expect(solver.factorial(5)).to be_a_kind_of(Integer)
       end
 
       it "should raise an exception for negative numbers" do
-        expect { Solver.factorial(-1) }.to raise_error(ArgumentError)
+        expect { solver.factorial(-1) }.to raise_error(ArgumentError)
       end
 
       it "should return 1 for 0" do
-        expect(Solver.factorial(0)).to eq(1)
+        expect(solver.factorial(0)).to eq(1)
       end
 
       it "should return 1 for 1" do
-        expect(Solver.factorial(1)).to eq(1)
+        expect(solver.factorial(1)).to eq(1)
       end
 
       it "should return 24 for 4" do
-        expect(Solver.factorial(4)).to eq(24)
+        expect(solver.factorial(4)).to eq(24)
       end
     end
   end
